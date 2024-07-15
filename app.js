0e303d275d6f2d865b612702c1c171ab091d2443
Muhammad Hasnain Khatri Attari <hasnainkhatri45@gmail.com>
2024-07-15 06:08:23 -0700

    half

---
+++ app.js
@@ -0,0 +1,10 @@
+var submit= document.getElementById('submit')
+
+
+var fullName = document.getElementById('fn').value
+var age = document.getElementById('age').value
+var number = document.getElementById('number').value
+var gender = document.getElementById('gender').value
+var cnic = document.getElementById('cnic').value
+
+
